567ddad1b95f2ee87f743e47bb765c9bd9177db6
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-26 09:29:47 -0400

    Add todo container component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,34 @@
+import React from "react"
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+
+  render() {
+    return (
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoContainer
