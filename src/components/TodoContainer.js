f012e717159c7462d672b4b376a6a97f41e37101
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-27 02:56:17 -0400

    Add Todo item insertion handler

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import TodoList from "./TodoList";
 import Header from "./Header"
+import InputTodo from "./InputTodo"
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -46,10 +47,22 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = title => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodoList todos={this.state.todos}
         handleChangeProps={this.handleChange}
         deleteTodoProps={this.delTodo} />
