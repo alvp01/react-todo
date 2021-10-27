c88e300b94b7ee32e87ae4e85cbd5dfa95005de9
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-27 03:18:29 -0400

    Add inline style for Todo completion

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,17 +3,24 @@ import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    const { completed, id, title } = this.props.todo
+
     return <li className={styles.item}>
       <input
         type="checkbox"
         className={styles.checkbox}
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        checked={completed}
+        onChange={() => this.props.handleChangeProps(id)}
       />
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-        Delete
-      </button>
-      {this.props.todo.title}
+      <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
     </li>
   }
 }
