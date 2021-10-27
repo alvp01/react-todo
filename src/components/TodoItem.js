62726b40fdaea3232ce1903e70590350e0696675
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-27 03:12:03 -0400

    Add style module to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,20 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
-    return <li><input type="checkbox" 
-    checked={this.props.todo.completed} 
-    onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
-    {this.props.todo.title}
-    <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-      Delete
-    </button></li>
+    return <li className={styles.item}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={this.props.todo.completed}
+        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      />
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
+      {this.props.todo.title}
+    </li>
   }
 }
 
