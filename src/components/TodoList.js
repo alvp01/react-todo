963e2f08ece240608a5d00e028da8a3199c1947b
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-27 04:54:26 -0400

    Change TodoList component to be function based

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,19 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from "react"
+import TodoItem from "./TodoItem"
 
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo}
-          handleChangeProps={this.props.handleChangeProps}
-          deleteTodoProps={this.props.deleteTodoProps}
-          setUpdate={this.props.setUpdate} />
-        ))}
-      </ul>
-    )
-  }
+const TodoList = props => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  )
 }
-
 export default TodoList
